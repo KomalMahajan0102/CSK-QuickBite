230f8e9327d160d72911a355f35a9a89721776c1
KomalMahajan0102 <127200045+KomalMahajan0102@users.noreply.github.com>
2025-06-08 14:54:23 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider=(props)=>{
     const [cartItems,setCartItems]=useState({});
     const [token,setToken]=useState("");
     const [food_list,setFoodList]=useState([]);
-    const url="http://localhost:4000";
+    const url="https://csk-quickbite-api.onrender.com";
     const addToCart=async(itemId)=>{
         if(!cartItems[itemId]){
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
